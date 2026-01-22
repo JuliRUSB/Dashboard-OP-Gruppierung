63eb5ae8d383419107248a3c9c95f3ee3e06074d
JuliRUSB <julia.ruppel@usb.ch>
2026-01-22 14:48:02 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -456,7 +456,8 @@ with tab4:
         
         fig_dindo.update_traces(
             # Textposition kann hier entfernt oder angepasst werden, da es bei "group" komplex wird
-            marker_line_width=0
+            marker_line_width=0,
+            width=0.9
         )
         
         fig_dindo.update_layout(
