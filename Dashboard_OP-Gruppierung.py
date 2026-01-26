8b90e9a30e31007ffbd585766d77032bf2eca244
JuliRUSB <julia.ruppel@usb.ch>
2026-01-26 13:10:45 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -261,6 +261,19 @@ with st.sidebar:
         st.session_state.selected_quartale = quartal_filter
 
     st.divider()
+
+    widgets.IntRangeSlider(
+    value=[5, 7],
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d',
+)
     
     # Bereich-Filter (Dropdown)
     bereich_filter = st.selectbox(
