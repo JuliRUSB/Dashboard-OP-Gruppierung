9bed3c6c018798aa3e83b65aad02d63c02a4af17
JuliRUSB <julia.ruppel@usb.ch>
2026-02-13 12:13:37 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -622,7 +622,8 @@ for i, bereich in enumerate(bereiche):
             
                             fig.update_traces(
                                 textfont_size=16, 
-                                textposition='auto', # Deine gewünschte Einstellung
+                                textposition='auto',
+                                name="",
                                 marker_line_width=0
                             )
         
