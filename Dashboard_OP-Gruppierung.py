e62d9e774f05fc6d58d58a09967cc604a4bc196d
JuliRUSB <julia.ruppel@usb.ch>
2026-02-13 08:34:53 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -598,45 +598,45 @@ for i, bereich in enumerate(bereiche):
         # ================== BEREICH CHURURGISCHE ONKOLOGIE/SARKOME ================== 
 
         # ================== Reiter Übersicht Sarkome ================== 
-        if "Gesamtzahl Operationen" in analysen:
-            with tabs[analysen.index("Gesamtzahl Operationen")]:
-                if "bereich" in df_bereich.columns and df_bereich["bereich"].nunique() > 0:
+        #if "Gesamtzahl Operationen" in analysen:
+            #with tabs[analysen.index("Gesamtzahl Operationen")]:
+                #if "bereich" in df_bereich.columns and df_bereich["bereich"].nunique() > 0:
 
                     # Filter auf bereich = '4'
-                    df_plot = df_bereich[df_bereich["bereich"] == 'Chirurgische Onkologie/Sarkome'].copy()
+                    #df_plot = df_bereich[df_bereich["bereich"] == 'Chirurgische Onkologie/Sarkome'].copy()
 
-                    if df_plot.empty:
-                        st.info("Keine Daten für Chirurgische Onkologie/Sarkome")
-                    else:
+                    #if df_plot.empty:
+                        #st.info("Keine Daten für Chirurgische Onkologie/Sarkome")
+                    #else:
                         # Gruppieren und count berechnen
-                        grp = df_plot.groupby(["jahr_opdatum", "bereich"]).size().reset_index(name="count")
+                        #grp = df_plot.groupby(["jahr_opdatum", "bereich"]).size().reset_index(name="count")
 
-                    fig = px.bar(
-                        grp,
-                        x="jahr_opdatum",
-                        y="count",
-                        color="bereich",
-                        barmode="group",
-                        text="count",
-                        color_discrete_sequence=COLOR_PALETTE
-                    )
+                    #fig = px.bar(
+                        #grp,
+                        #x="jahr_opdatum",
+                        #y="count",
+                        #color="bereich",
+                        #barmode="group",
+                        #text="count",
+                        #color_discrete_sequence=COLOR_PALETTE
+                    #)
                     
-                    fig.update_traces(
-                        textfont_size=16, 
-                        textposition='inside'
-                    )
+                    #fig.update_traces(
+                        #textfont_size=16, 
+                        #textposition='inside'
+                    #)
 
-                    fig.update_layout(
-                        xaxis_title=None, 
-                        yaxis_title=None, 
-                        showlegend=False,
-                        xaxis={"type": "category", "tickfont": {"size": 16}}, # Verhindert Zahlensalat auf der X-Achse
-                        yaxis={"tickfont": {"size": 16}} 
-                    )
+                    #fig.update_layout(
+                        #xaxis_title=None, 
+                        #yaxis_title=None, 
+                        #showlegend=False,
+                        #xaxis={"type": "category", "tickfont": {"size": 16}}, # Verhindert Zahlensalat auf der X-Achse
+                        #yaxis={"tickfont": {"size": 16}} 
+                    #)
                     
-                    st.plotly_chart(fig, use_container_width=True)
-                else:
-                    st.info("Keine Daten")
+                    #st.plotly_chart(fig, use_container_width=True)
+                #else:
+                    #st.info("Keine Daten")
         
         # ================== Reiter Übersicht Sarkome ================== 
         if "Übersicht Sarkome" in analysen:
