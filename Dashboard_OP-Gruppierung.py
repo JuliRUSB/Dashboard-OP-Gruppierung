9fd3c2b9e332505cae1f5ec3760385128969b99f
JuliRUSB <julia.ruppel@usb.ch>
2026-02-19 15:28:40 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -570,55 +570,7 @@ for i, bereich in enumerate(bereiche):
         col1, col2, col3 = st.columns(3)
 
         # ================== Kachel 1 "Gesamtanzahl Operationen" ==================
-        with col0.container(border=True):
-            # Vorab-Check der Analyse-Auswahl
-            if "Gesamtzahl Operationen" in analysen:
-                # Sicherstellen, dass notwendige Spalten vorhanden sind
-                required_cols = {"bereich", "jahr_opdatum"}
-                if required_cols.issubset(df_bereich.columns):
-            
-                    # Effiziente Filterung
-                    df_plot = df_bereich[df_bereich["bereich"] == 'Chirurgische Onkologie/Sarkome'].copy()
-                    total_ops = len(df_plot)
-            
-                    st.metric(label="Gesamtzahl Operationen (Onkologie/Sarkome)", value=total_ops)
-                    st.divider()
-            
-                    if total_ops > 0:
-                        # Aggregation
-                        grp = df_plot.groupby("jahr_opdatum").size().reset_index(name="count")
-                
-                        fig = px.bar(
-                            grp,
-                            x="jahr_opdatum",
-                            y="count",
-                            text="count",
-                            color_discrete_sequence=COLOR_PALETTE
-                        )
-            
-                        fig.update_traces(
-                            textfont_size=16, 
-                            textposition='auto',
-                            marker_line_width=0 # Cleaner Look
-                        )
-                
-                        fig.update_layout(
-                            #height=450, # Feste Höhe für Dashboard-Kacheln
-                            margin=dict(l=10, r=10, t=0, b=10),
-                            xaxis_title=None, 
-                            yaxis_title=None, 
-                            showlegend=False,
-                            xaxis={"type": "category", "tickfont": {"size": 16}},
-                            yaxis={"showticklabels": True, "showgrid": True, "tickfont": {"size": 16}} 
-                        )
-            
-                        st.plotly_chart(fig, use_container_width=True, key="chart_ops_onkologie", config={'displayModeBar': False})
-                    else:
-                        st.info("Keine Daten für diesen Bereich gefunden.")
-                else:
-                    st.error("Fehlende Spalten im Datensatz.")
-            else:
-                st.metric(label="Gesamtzahl Operationen", value="-")
+        
 
             # ================== Kachel 3 HIPEC bei CRS ================== 
                 #DEBUGGING: um zu schauen, wie die Werte angezeigt werden
