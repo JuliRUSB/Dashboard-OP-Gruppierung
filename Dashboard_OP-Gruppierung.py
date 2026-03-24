0d766739ddadd6cfd82d34f04c12b1cad4f1b6fd
JuliRUSB <julia.ruppel@usb.ch>
2026-03-24 15:45:46 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -1202,14 +1202,6 @@ for i, bereich in enumerate(bereiche):
                         st.info("Keine Daten für Sarkom/Weichteiltumor")
             else:
                 st.error("Spalten fehlen")  
-
-        # Horizontale Trennlinie zur thematischen Abgrenzung 
-        st.markdown(
-            """
-            <hr style="border: 2px solid #d3d3d3; margin: 20px 0;">
-            """,
-            unsafe_allow_html=True
-        )
         
         # Zwei Spalten/Kacheln definieren (5. Reihe)
         col1, col2 = st.columns(2)
