52679913fda0ae1c7ada82388621636c4d6a6118
JuliRUSB <julia.ruppel@usb.ch>
2026-03-25 09:58:06 +0100

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -1549,8 +1549,8 @@ for i, bereich in enumerate(bereiche):
             if required_cols.issubset(df_bereich.columns):
             
                 # Filter für Anastomosen
-                df_plot = df_bereich[(df_bereich["anastomosen_crs"] == "Kolon") & (df_bereich["anastomosen_crs"] == "Rektum")].copy()
-                total_crs = len(df_plot)
+                df_plot = df_bereich[(df_bereich["anastomosen_crs"] == "Kolon") | (df_bereich["anastomosen_crs"] == "Rektum")].copy()
+                total_anastomosen = len(df_plot)
             
                 st.metric(label="Anastomosen bei CRS (Kolon und Rektum)", value=total_anastomosen)
                 st.divider()
