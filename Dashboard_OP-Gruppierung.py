6ad8c4ef6fad8295e2a5c6d6fd21a7948162f379
JuliRUSB <julia.ruppel@usb.ch>
2026-04-08 14:39:56 +0200

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -996,13 +996,6 @@ for i, bereich in enumerate(bereiche):
                     st.info("Keine Daten für HIPEC")
             else:
                 st.error("Spalten fehlen")
-
-        st.markdown(
-            """
-            <hr style="border: 2px solid #d3d3d3; margin: 20px 0;">
-            """,
-            unsafe_allow_html=True
-        )  # horizontale Linie
         
         # Zwei Spalten/Kacheln definieren (4. Reihe)
         col1, col2 = st.columns(2)
@@ -1177,6 +1170,13 @@ for i, bereich in enumerate(bereiche):
             else:
                 st.error("Spalten fehlen")
 
+        st.markdown(
+            """
+            <hr style="border: 2px solid #d3d3d3; margin: 20px 0;">
+            """,
+            unsafe_allow_html=True
+        )  # horizontale Linie
+        
         # Zwei Spalten/Kacheln definieren (5. Reihe)
         col1, col2 = st.columns(2)
     
