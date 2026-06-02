a572458908f4ac87c9eb4f729d31d0cd8bf9e0f4
JuliRUSB <julia.ruppel@usb.ch>
2026-06-02 14:22:04 +0200

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -987,7 +987,7 @@ for i, bereich in enumerate(BEREICHE):
         
                         fig.update_layout(
                             autosize=True,
-                            height=None,
+                            height=400,
                             bargap=0.1,
                             margin=dict(l=10, r=10, t=30, b=10),
                             xaxis_title=None,
