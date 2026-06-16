98a07660989d14ac9cf0a12146fdd7453c58603a
JuliRUSB <julia.ruppel@usb.ch>
2026-06-16 13:50:49 +0200

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -2,12 +2,12 @@
 # Imports – Bibliotheken laden
 # ==================================================
 
+import streamlit as st                           # Streamlit für Web-App
 import os                                        # Zugriff auf Umgebungsvariablen (z.B. API-Tokens)
 import requests                                  # HTTP-Requests (hier für REDCap API)
 import pandas as pd                              # Datenverarbeitung mit DataFrames
 st.write("Pandas-Version:", pd.__version__)
 import plotly.express as px                      # Plotly Express für Diagramme
-import streamlit as st                           # Streamlit für Web-App
 import urllib3                                   # Bibliothek für HTTP-Kommunikation
 import plotly.graph_objects as go                # Low-Level-Schnittstelle von Plotly
 import streamlit.components.v1 as components     # Modul von Streamlit, mit dem man HTML/JavaScript-Code direkt im Browser ausführen kann
