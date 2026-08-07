8461a3e286442168e7984455ded16ac1418b37e1
JuliRUSB <julia.ruppel@usb.ch>
2026-08-07 16:20:13 +0200

    Update Dashboard_OP-Gruppierung.py

--- Dashboard_OP-Gruppierung.py
+++ Dashboard_OP-Gruppierung.py
@@ -2974,7 +2974,7 @@ for i, bereich in enumerate(BEREICHE):
                                 margin=dict(l=10, r=10, t=0, b=10),
                                 xaxis_title=None, 
                                 yaxis_title=None, 
-                                showlegend=false,
+                                showlegend=False,
                                 xaxis={"type": "category", "tickfont": {"size": 16}},
                                 yaxis={"showticklabels": True, "showgrid": True, "tickfont": {"size": 16}} 
                             )
